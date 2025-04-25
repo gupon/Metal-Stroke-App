10258a7d4e7877224fe05b532974bd3bb3a3fa19
Takuma Miyamoto <me@gupon.jp>
2025-04-25 18:05:06 +0900

    Initial Commit

---
+++ 250425MetalTriangle/_50425MetalTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _50425MetalTriangleApp.swift
+//  250425MetalTriangle
+//
+//  Created by Takuma Miyamoto on 2025/04/25.
+//
+
+import SwiftUI
+
+@main
+struct _50425MetalTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
